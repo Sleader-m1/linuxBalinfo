74e74b248189aa757b14f54a7a66e85cd1a223df
sleader <shamil.abdllv@gmail.com>
2023-09-12 18:18:42 +0300

    altlinux

--- getPCInfo.py
+++ getPCInfo.py
@@ -61,7 +61,7 @@ start_time = time.time()
 
 def get_serial_number():
     try:
-        output = subprocess.check_output(["sudo", "dmidecode", "-s", "system-serial-number"])
+        output = subprocess.check_output([ "dmidecode", "-s", "system-serial-number"])
         serial_number = output.decode().strip()
         return serial_number
     except subprocess.CalledProcessError:
@@ -95,6 +95,7 @@ def getLocalIP():
 
 
 def get_package_list():
+    return [{"name":"hui", "version":"1"}]
     command = "dpkg -l"
 
     try:
@@ -114,7 +115,7 @@ def getCPUName():
 
 
 def getARMInfo():
-    command = "sudo lshw"
+    command = "lshw"
 
     try:
         output = subprocess.check_output(command, shell=True).decode("utf-8").strip().split("\n")[1:4]
@@ -178,69 +179,69 @@ def getFullInformation():
     try:
         arm = getARMInfo()
     except:
-        Error_messages.append('Error while collecting ARM info!')
+        Error_messages.append({"message": 'Error while collecting ARM info!'})
 
     try:
         serial_number = get_serial_number()
     except:
-        Error_messages.append('Error while collecting serisl number!')
+        Error_messages.append({"message": 'Error while collecting serisl number!'})
 
     try:
         osType = getOS()
     except:
-        Error_messages.append('Error while collecting OS type!')
+        Error_messages.append({"message": 'Error while collecting OS type!'})
 
     try:
         osName = getOSType()
     except:
-        Error_messages.append('Error while collecting OS name!')
+        Error_messages.append({"message": 'Error while collecting OS name!'})
 
     try:
         hostName = getHostname()
     except:
-        Error_messages.append('Error while collecting host name!')
+        Error_messages.append({"message": 'Error while collecting host name!'})
 
     try:
         userName = getUsername()
     except:
-        Error_messages.append('Error while collecting user name!')
+        Error_messages.append({"message": 'Error while collecting user name!'})
 
     try:
         localIP = getLocalIP()
     except:
-        Error_messages.append('Error while collecting local IP!')
+        Error_messages.append({"message": 'Error while collecting local IP!'})
 
     try:
         globalIP = getGlobalIP()
     except:
-        Error_messages.append('Error while collecting global IP!')
+        Error_messages.append({"message": 'Error while collecting global IP!'})
 
     try:
         macAddress = getMAC()
     except:
-        Error_messages.append('Error while collecting MAC address!')
+        Error_messages.append({"message": 'Error while collecting MAC address!'})
 
     try:
         cpuName = getCPUName()
     except:
-        Error_messages.append('Error while collecting CPU name!')
+        Error_messages.append({"message": 'Error while collecting CPU name!'})
 
     try:
         ramSpace = getRAMSpace()
     except:
-        Error_messages.append('Error while collecting RAM space!')
+        Error_messages.append({"message": 'Error while collecting RAM space!'})
 
     try:
         applications = get_package_list()
     except:
-        Error_messages.append('Error while collecting applications!')
+        Error_messages.append({"message": 'Error while collecting applications!'})
 
     try:
         disks = getDisks()
     except:
-        Error_messages.append('Error while collecting disks!')
+        Error_messages.append({"message": 'Error while collecting disks!'})
 
-
+    print(Error_messages)
     result = {
         "Uptime" : str({round(time.time() - start_time, 1)}),
         "Access_Token": json_config["token"],
@@ -286,12 +287,12 @@ def cicleRequest():
     try:
         applications = get_package_list()
     except:
-        error_messages.append('Error while collecting applications!')
+        error_messages.append({"message": 'Error while collecting applications!'})
 
     try:
         disks = getDisks()
     except:
-        error_messages.append('Error while collecting disks!')
+        error_messages.append({"message": 'Error while collecting disks!'})
 
     result = {
         "Uptime" : str({round(time.time() - start_time, 1)}),
@@ -354,7 +355,7 @@ def authorize():
 
 def main():
     authorize()
-    autostart()
+    #autostart()
     while not primaryPOSTRequest():
         print('Trying primary again!')
         time.sleep(300)
